1b06fcb7a602ec0b5c9deb558c4d83e03a260fab
Fadzai Jongwe <fjon0006@student.monash.edu>
2026-04-02 00:52:23 +1100

    Add support for subtraction

--- calc.py
+++ calc.py
@@ -1,11 +1,11 @@
 def compute(expression):
     values = expression.split(' ')
     num0 = int(values[0])
-    operator = values[1]
+    operator1 = values[1]
     num1 = int(values[2])
-    if operator == '+':
+    if operator1 == '+':
         return num0 + num1
-    elif operator == '-':
+    elif operator1 == '-':
         return num0 - num1
     else:
         print('unknown operator!')
